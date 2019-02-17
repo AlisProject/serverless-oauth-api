72970b29795bbdf63ab16cf92299e3456796823e
horike37 <horike37@gmail.com>
2019-02-18 01:20:08 +0900

    update

--- sh/move-deployment-packge.sh
+++ sh/move-deployment-packge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Moving the deployment package to S3 Bucket"
-npx serverless package --s3-bucket ${DEPLOYMENT_BUCKET} --s3-key ${CIRCLE_TAG}
+npx serverless package --s3-bucket ${DEPLOYMENT_BUCKET} --s3-key ${CIRCLE_TAG} --stage production
 aws s3 sync .serverless/ s3://${DEPLOYMENT_BUCKET}/${CIRCLE_TAG}
